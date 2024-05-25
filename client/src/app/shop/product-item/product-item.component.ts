96bfd48269c1929d2567db1871bd31f7f4428523
Sheikh Saqib <sheikhsaqib40@gmail.com>
2024-05-25 20:31:26 +0100

    Added Products Component

---
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IProduct } from '../../models/product';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrl: './product-item.component.scss',
+})
+export class ProductItemComponent implements OnInit {
+  @Input() product: IProduct | undefined;
+  constructor() {}
+
+  ngOnInit() {}
+}
