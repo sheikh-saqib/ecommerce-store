66400bdfd552f4c4cb1d7aabb528d78b4f3116a8
Sheikh Saqib <sheikhsaqib40@gmail.com>
2024-06-06 21:50:42 +0100

    Added Pager

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { PagingHeaderComponent } from './paging-header/paging-header.component';
+import { PagerComponent } from './pager/pager.component';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PagingHeaderComponent, PagerComponent],
+  imports: [CommonModule, PaginationModule.forRoot(), FormsModule],
+
+  exports: [PagingHeaderComponent, PagerComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
+import { SharedModule } from '../shared/shared.module';
+import { ShopRoutingModule } from './shop-routing.module';
 
 @NgModule({
   declarations: [ShopComponent, ProductItemComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule, ShopRoutingModule],
   exports: [ShopComponent],
 })
 export class ShopModule {}
